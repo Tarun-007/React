c76b17cf906fe465652651fd1f2a90a4554a9739
TarunJuluru-nc14977 <tarunjuluru@ninjacart.com>
2021-05-30 12:45:16 +0530

    added delete functionality

--- src/Cart.js
+++ src/Cart.js
@@ -3,12 +3,88 @@ import CartItem from './CartItem'
 
 
 class Cart extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            products: [
+                {
+                    price: 99,
+                    quantity: 1,
+                    item: 'Watch',
+                    img: '',
+                    id: 1
+                },
+                {
+                    price: 999,
+                    quantity: 4,
+                    item: 'Mobile Phone',
+                    img: '',
+                    id: 2
+                },
+                {
+                    price: 400,
+                    quantity: 7,
+                    item: 'Mobile case',
+                    img: '',
+                    id: 3
+                }
+            ]
+        }
+    }
+
+    handlerDeleteItem = (product) => {
+        console.log('removing the item');
+        const products = this.state.products;
+        const index = this.state.products.indexOf(product);
+        products.splice(index, 1);
+        this.setState(() => {
+            return {
+                products : products
+            }
+        })
+    }
+    
+    handlerIncreaseQuantity = (product) => {
+        console.log("increasing quantity of product ", product)
+        const products = this.state.products;
+        const index = products.indexOf(product);
+        products[index].quantity += 1
+        this.setState(() => {
+            return {
+                products : products
+            }
+        })
+    }
+    handlerDecreaseQuantity = (product) => {
+        console.log("increasing quantity of product ", product)
+        const products = this.state.products;
+        const index = products.indexOf(product);
+        if (products[index].quantity > 0) {
+            products[index].quantity -= 1
+            this.setState(() => {
+                return {
+                    products: products
+                }
+            })
+        }
+    
+    }
     render() {
+        const { products } = this.state;
         return (
-            <div className = "cart">
-                <CartItem />
-                <CartItem />
-                <CartItem />
+            <div className="cart">
+                {products.map((product) => {
+                    return (
+                        <CartItem
+                        product={product}
+                        key={product.id}
+                            handlerIncreaseQuantity={this.handlerIncreaseQuantity}
+                            handlerDecreaseQuantity={this.handlerDecreaseQuantity}
+                            handlerDeleteItem = {this.handlerDeleteItem}
+                        />
+                    )
+                })
+                }
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -4,56 +4,11 @@ import React from 'react';
 
 class CartItem extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {
-            price: 999,
-            quantity: 1,
-            item: 'Mobile Phone',
-            img: ''
-        }
-    }
-
-    increaseQuantity = () => {
-        // if previous state not required use this
-        // this.setState({
-        //     quantity: this.state.quantity + 1
-        // }, () => {
-        //         console.log(this.state.quantity)
-        // })
-        
-
-        //if previous state required use this
-        this.setState((prevState) => {
-            return {
-                quantity: prevState.quantity + 1
-            }
-        },
-            () => {
-                console.log(this.state.quantity)
-        });
-    
-        
-        
-        
-        
-
-    
-    }
-
-    decreaseQuantity = () => {
-        this.setState((prevState) => {
-            if (prevState.quantity > 0) {
-                return {
-                    quantity: prevState.quantity - 1
-                }
-            }
-        })
-    }
 
     render() {
 
-        const { price, quantity, item, img } = this.state
+        const { product, handlerIncreaseQuantity, handlerDecreaseQuantity, handlerDeleteItem } = this.props;
+
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -62,32 +17,32 @@ class CartItem extends React.Component {
 
                 <div className="right-block">
                     <div style={{ fontSize: 25 }}>
-                        {item}
+                        {product.name}
                     </div>
                     <div style={{ color: 'grey' }}>
-                        Rs {price}
+                        Rs {product.price}
                     </div>
                     <div style={{ color: 'grey' }}>
-                        Qty : {quantity}
+                        Qty : {product.quantity}
                     </div>
                     <div className="cart-item-actions">
                         <img
                             className="action-icons"
                             src="https://image.flaticon.com/icons/png/512/992/992651.png"
                             alt="increase"
-                            onClick={this.increaseQuantity}
+                            onClick={()=>handlerIncreaseQuantity(product)}
                         />
                         <img
                             className="action-icons"
                             src="https://image.flaticon.com/icons/png/512/992/992683.png"
                             alt="decrease"
-                            onClick = {this.decreaseQuantity.bind(this)}
+                            onClick = {()=>handlerDecreaseQuantity(product)}
                         />
                         <img
                             className="action-icons"
                             src="https://image.flaticon.com/icons/png/512/1214/1214428.png"
                             alt="remove"
-                            // onClick = {this.delelt}
+                             onClick = {()=>handlerDeleteItem(product)}
                         />
                     </div>
                 </div>
